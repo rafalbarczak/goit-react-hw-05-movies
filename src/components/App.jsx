92458a6815a7e106e2b8d89f0989b45c3ca50b2e
rafalbarczak <rafal.barczak@intmail.pl>
2023-09-18 05:43:34 +0200

    added simple api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import { fetchMovies } from './api';
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  fetchMovies(2);
+  return <div>=</div>;
 };
---
+++ src/components/api.js
@@ -0,0 +1,20 @@
+export async function fetchMovies(page) {
+  const API_KEY = '5577624f107f96fb286666953fd25f4f';
+
+  return fetch(
+    `https://api.themoviedb.org/3/trending/all/day?page=${page}&api_key=${API_KEY}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      console.log(response.json());
+      //   return response.json();
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
